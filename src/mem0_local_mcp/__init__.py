4e31f7fd17c354f6bb4a07be1bc77436b941fea2
ldelitala <ldelitala@gmail.com>
2026-04-16 18:13:27 +0200

    chore: initialize src directory structure

---
+++ src/mem0_local_mcp/__init__.py
@@ -0,0 +1,5 @@
+from .mcp_server import mcp, add_memory, search_memory
+from .mem_manager import MemManager
+from .utils import get_project_id
+
+__all__ = ["mcp", "add_memory", "search_memory", "MemManager", "get_project_id"]
